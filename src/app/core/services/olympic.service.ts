b927d51e3e6ce95341c4bcff41f555551d42aeb1
TangiLC <tangi@le-cadre.net>
2024-11-11 16:12:38 +0100

    home component

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,7 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
+
+interface CountryData {
+  name: string;
+  totalParticipations: number;
+  totalMedalCount: number[];
+}
 
 @Injectable({
   providedIn: 'root',
@@ -9,17 +15,25 @@ import { catchError, tap } from 'rxjs/operators';
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
   private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympicStats$ = new BehaviorSubject<{
+    countryData: CountryData[];
+    maxTotalParticipations: number;
+  } | null>(null);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
     return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
+      tap((value) => {
+        this.olympics$.next(value);
+        this.calculateStats(value);
+      }),
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next(null);
+        this.olympicStats$.next({ countryData: [], maxTotalParticipations: 0 });
         return caught;
       })
     );
@@ -27,5 +41,37 @@ export class OlympicService {
 
   getOlympics() {
     return this.olympics$.asObservable();
+  }
+
+  private calculateStats(olympics: any) {
+    if (!olympics) {
+      this.olympicStats$.next({ countryData: [], maxTotalParticipations: 0 });
+      return;
+    }
+    const countryData = olympics.map((country: any) => {
+      const name = country.country;
+      const totalParticipations = country.participations.length;
+      const totalMedalCount = country.participations.reduce(
+        (acc: number[], participation: any) => {
+          return [
+            acc[0] + participation.medalsCount[0], // Or
+            acc[1] + participation.medalsCount[1], // Argent
+            acc[2] + participation.medalsCount[2], // Bronze
+          ];
+        },
+        [0, 0, 0]
+      );
+
+      return { name, totalParticipations, totalMedalCount };
+    });
+    const maxTotalParticipations = Math.max(
+      ...countryData.map((country: any) => country.totalParticipations)
+    );
+
+    this.olympicStats$.next({ countryData, maxTotalParticipations });
+  }
+
+  getOlympicStats() {
+    return this.olympicStats$.asObservable();
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,10 +9,19 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+  public olympicStats$: Observable<{
+    countryData: {
+      name: string;
+      totalParticipations: number;
+      totalMedalCount: number[];
+    }[];
+    maxTotalParticipations: number;
+  } | null> = of({ countryData: [], maxTotalParticipations: 0 });
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympicStats$ = this.olympicService.getOlympicStats();
   }
 }
